53518b34b437c7cbe514a45781b27d704453df37
geghamgrigoryan2005 <gegham.grigoryan2005@gmail.com>
2026-07-23 17:27:37 +0400

    23-07

--- main.js
+++ main.js
@@ -12,28 +12,34 @@ function updateTaskNumber() {
 }
 
 function addTask() {
-  if (taskInput.value.trim().length < 1) {
+  let inputText = taskInput.value.trim();
+  if (inputText.length < 1) {
     alert("Cannot be empty");
     return;
   }
   let toDo = document.createElement("li");
   let cancelButton = document.createElement("button");
   cancelButton.textContent = "X";
-  cancelButton.addEventListener("click", removeTask);
-  taskList.append(toDo, cancelButton);
-  cancelButton.classList.add("denyButton");
+
+  taskList.append(toDo);
+
+  
+  cancelButton.classList.add("denyButton","dark");
   toDo.classList.add("listItem");
-  toDo.textContent = taskInput.value;
+  toDo.innerHTML = `
+     <p class="red"> Work-task <span>${inputText}</span></p>
+  
+  `;
   tasks++;
   updateTaskNumber();
   taskInput.value = "";
+  toDo.append(cancelButton);
+  cancelButton.addEventListener("click", removeTask);
 }
 
-function removeTask() {
-  let toDo = document.querySelector(".listItem");
-  let cancelButton = document.querySelector(".denybutton");
+function removeTask(event) {
+  event.target.parentElement.remove();
   tasks--;
-  count.textContent = tasks;
-  toDo.remove();
-  cancelButton.remove();
+  updateTaskNumber();
+  console.log(event.target);
 }
